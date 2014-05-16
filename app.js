62143b8cf24be2198221cc8e420747ec43abad52
Atul Varma <varmaa@gmail.com>
2014-05-16 10:09:03 -0400

    remove unused const

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var MAKES_URL_RE = /^https:\/\/[A-Za-z0-9_\-]+\.makes\.org\//;
 var ENDS_WITH_UNDERSCORE_RE = /_$/;
 var PORT = process.env.PORT || 3000;
 var PHANTOMJS = process.env.PHANTOMJS || 'phantomjs';
-var COOKIE_SECRET = process.env.COOKIE_SECRET || 'meh';
 var BLITLINE_APPLICATION_ID = process.env.BLITLINE_APPLICATION_ID;
 
 var app = express();
