d05423ba6cdbf353191f5a4c3ee3a8bfb66fc410
Atul Varma <varmaa@gmail.com>
2014-05-16 10:06:49 -0400

    factored out getScreenshot() function.

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,31 @@
 var form = document.querySelector('form');
 
-form.addEventListener('submit', function(event) {
+function getScreenshot(url, cb) {
   var req = new XMLHttpRequest();
 
-  event.preventDefault();
   req.open('POST', window.location.pathname);
   req.setRequestHeader('Content-Type', 'application/json');
   req.responseType = 'json';
   req.addEventListener('load', function(event) {
     console.log('GOT', req.response);
-    var screenshot = req.response.screenshot;
-    if (!screenshot) return;
+    if (req.response && req.response.error)
+      return cb(new Error(req.response.error));
+    else if (req.status != 200)
+      return cb(new Error('Got HTTP ' + req.status));
+    cb(null, req.response.screenshot);
+  }, false);
+  req.send(JSON.stringify({url: url}));
+}
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  getScreenshot(form.elements.url.value, function(err, screenshot) {
+    if (err) {
+      alert(err.message);
+      return;
+    }
     var img = new Image(screenshot.width, screenshot.height);
     img.src = screenshot.url;
     document.body.appendChild(img);
-  }, false);
-  req.send(JSON.stringify({url: form.elements.url.value}));
+  });
 }, false);
