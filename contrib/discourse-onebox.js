ce3d1cdd3763fd32c57e9b7eb2d5e9cee918868c
Atul Varma <varmaa@gmail.com>
2014-05-19 08:32:36 -0400

    use a real onebox

--- contrib/discourse-onebox.js
+++ contrib/discourse-onebox.js
@@ -7,14 +7,27 @@ Discourse.PostView.reopen({
 
     this.$('a').each(function() {
       var href = $(this).attr('href');
-      var isLinkOnItsOwnLine = (this.parentNode.nodeName == 'P' &&
-                                $(this.parentNode).children().length == 1);
+      var parent = this.parentNode;
+      var isLinkOnItsOwnLine = (parent.nodeName == 'P' &&
+                                $(parent).children().length == 1);
       if (!(MAKES_URL_RE.test(href) && isLinkOnItsOwnLine)) return;
+
+      var onebox = $(
+        '<div class="onebox-result">' +
+        '<div class="source"><div class="info"></div></div>' +
+        '<div class="onebox-result-body">' +
+        '<div class="clearfix"></div>' +
+        '</div>' +
+        '</div>'
+      );
+
+      $('.info', onebox).append(this);
 
       var img = document.createElement('img');
       img.setAttribute('src', BASE_SCREENSHOT_URL + href.slice(8));
-      img.setAttribute('style', 'display: block');
-      $(this).prepend(img);
+      $('.onebox-result-body', onebox).prepend(img);
+
+      $(parent).replaceWith(onebox);
     });
   }
 });
