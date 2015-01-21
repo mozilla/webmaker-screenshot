6a05bd4c64e063a9554667e6d33bdeb04bab0978
Atul Varma <varmaa@gmail.com>
2015-01-21 16:17:38 -0500

    more tests.

--- test/redis-cache.test.js
+++ test/redis-cache.test.js
@@ -3,6 +3,10 @@ var should = require('should');
 
 var RedisCache = require("../redis-cache");
 
+function noCache() {
+  throw new Error("this should never be called");
+}
+
 describe("RedisCache", function() {
   var cache, client;
 
@@ -15,6 +19,31 @@ describe("RedisCache", function() {
     client.keys('TESTING_*', function(err, keys) {
       if (err) return done(err);
       keys.length ? client.del(keys, done) : done();
+    });
+  });
+
+  describe("get()", function() {
+    it("should use cached value if available", function(done) {
+      cache.get("blop", function cache(key, cb) {
+        cb(null, {thing: "yay " + key});
+      }, function(err, info) {
+        if (err) return done(err);
+        cache.get("blop", noCache, function(err, info) {
+          if (err) return done(err);
+          info.should.eql({thing: "yay blop"});
+          done();
+        });
+      });
+    });
+
+    it("should report JSON errors", function(done) {
+      client.set("TESTING_info_blah", "sup", function(err) {
+        if (err) return done(err);
+        cache.get("blah", noCache, function(err, info) {
+          err.toString().should.match(/unexpected token/i);
+          done();
+        });
+      });
     });
   });
 
@@ -62,20 +91,17 @@ describe("RedisCache", function() {
 
     it("should call retryMethod when lock is taken", function(done) {
       cache.lockAndSet("foo", function firstCache(key, cb) {
-        var secondCache = function(key, cb) {
-          throw new Error("this should not be called");
-        };
         var secondDone = function(err, info) {
-          throw new Error("this should not be called either");
+          throw new Error("this should not be called");
         };
         var retry = function(key, cacheCb, doneCb, retryMethod) {
           key.should.eql("foo");
-          cacheCb.should.equal(secondCache);
+          cacheCb.should.equal(noCache);
           doneCb.should.equal(secondDone);
           retryMethod.should.equal(retry);
           cb(null, "hooray");
         };
-        cache.lockAndSet("foo", secondCache, secondDone, retry);
+        cache.lockAndSet("foo", noCache, secondDone, retry);
       }, done);
     });
   });
