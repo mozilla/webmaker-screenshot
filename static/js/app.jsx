11fd30df1dd35ce9bba4964d67f30ff656b4ea55
Atul Varma <varmaa@gmail.com>
2015-01-23 18:20:38 -0500

    implement the regenerate button.

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -111,11 +111,79 @@ var Thumbnail = React.createClass({
 });
 
 var Viewport = React.createClass({
+  getInitialState: function() {
+    return {
+      regenerating: false
+    };
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (prevProps.url != this.props.url && this.state.regenerating) {
+      // It's pretty unlikely for this to happen, but just in case
+      // it does...
+      this.regenerateRequest = null;
+      this.setState({regenerating: false});
+    }
+  },
   handleRegenerate: function() {
-    alert("TODO: Implement this.");
+    var req = new XMLHttpRequest();
+    var mt = this.props.config.defaultThumbnail.forMake(this.props.url);
+    var url = '/' + mt.key;
+    var wait = this.refs.wait.getDOMNode().checked;
+
+    this.setState({
+      regenerating: true,
+      regenerateError: null
+    });
+    this.regenerateRequest = req;
+    req.open('POST', url);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.responseType = 'json';
+    req.addEventListener('load', this.onRegenerateDone, false);
+    req.send(JSON.stringify({wait: wait}));
+  },
+  onRegenerateDone: function(e) {
+    var req = this.regenerateRequest;
+    if (!this.isMounted() || e.target !== req) return;
+    console.log('GOT', req.response);
+    if (req.response && req.response.error)
+      return this.setState({
+        regenerateError: req.response.error
+      });
+    else if (req.status != 200)
+      return this.setState({
+        regenerateError: 'Got HTTP ' + req.status
+      });
+    this.setState({regenerating: false});
   },
   render: function() {
     var viewport = this.props.config;
+    var content;
+
+    if (this.state.regenerating) {
+      content = (
+        <div>
+          {this.state.regenerateError
+           ? <div className="alert alert-danger">
+               <p>Alas, an error occurred while regenerating screenshots for {viewport.slug}: <code>{this.state.regenerateError}</code></p>
+             </div>
+           : <p>Regenerating {viewport.slug} screenshots&hellip; <img src="/img/throbber.svg"/></p>}
+        </div>
+      );
+    } else {
+      content = (
+        <div>
+          {viewport.thumbnails.map(function(thumbnail) {
+            return <Thumbnail key={thumbnail.slug} config={thumbnail} url={this.props.url}/>;
+          }, this)}
+          <button className="btn btn-primary" onClick={this.handleRegenerate}>
+            <i className="fa fa-refresh"></i> Regenerate {viewport.slug} screenshots
+          </button>
+          <div className="checkbox">
+            <input ref="wait" type="checkbox"/> Wait a few extra seconds for JavaScript to load before taking the screenshots
+          </div>
+        </div>
+      );
+    }
 
     return (
       <div>
@@ -123,15 +191,7 @@ var Viewport = React.createClass({
         <div style={{marginTop: -10, marginBottom: 10}}>
           <small><span className="text-muted">{viewport.width}&times;{viewport.height} viewport</span></small>
         </div>
-        {viewport.thumbnails.map(function(thumbnail) {
-          return <Thumbnail key={thumbnail.slug} config={thumbnail} url={this.props.url}/>;
-        }, this)}
-        <button className="btn btn-primary" onClick={this.handleRegenerate}>
-          <i className="fa fa-refresh"></i> Regenerate {viewport.slug} screenshots
-        </button>
-        <div className="checkbox">
-          <input type="checkbox"/> Wait a few extra seconds for JavaScript to load before taking the screenshots
-        </div>
+        {content}
       </div>
     );
   }
