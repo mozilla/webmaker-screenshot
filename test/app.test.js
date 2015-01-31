a99e1408ccdeee86d8b00ba4e06878050a514f1a
Atul Varma <varmaa@gmail.com>
2015-01-30 20:39:46 -0500

    add tests for POST screenshot endpoint. fixes #5.

--- test/app.test.js
+++ test/app.test.js
@@ -62,113 +62,162 @@ describe("app", function() {
       if (s3) s3.done();
     });
 
-    it("return 404 for nonexistent makes", function(done) {
-      verifyIsHtml = function(url, cb) {
-        url.should.eql('https://t.makes.org/blah_');
-        cb(null, false);
-      };
+    describe("GET /:viewport/:thumbnail/:key", function() {
+      it("return 404 for nonexistent makes", function(done) {
+        verifyIsHtml = function(url, cb) {
+          url.should.eql('https://t.makes.org/blah_');
+          cb(null, false);
+        };
 
-      s3 = nock('https://s3.amazonaws.com')
-        .head('/FAKES3/desktop/large/t.makes.org/blah')
-        .reply(404);
+        s3 = nock('https://s3.amazonaws.com')
+          .head('/FAKES3/desktop/large/t.makes.org/blah')
+          .reply(404);
 
-      request(app)
-        .get('/desktop/large/t.makes.org/blah')
-        .expect(404)
-        .end(done);
+        request(app)
+          .get('/desktop/large/t.makes.org/blah')
+          .expect(404)
+          .end(done);
+      });
+
+      it("propagate errors when looking up makes", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(new Error("blah"));
+        };
+
+        s3 = nock('https://s3.amazonaws.com')
+          .head('/FAKES3/desktop/large/t.makes.org/blah')
+          .reply(404);
+
+        request(app)
+          .get('/desktop/large/t.makes.org/blah')
+          .expect(500)
+          .expect("blah")
+          .end(done);
+      });
+
+      it("return 200 for existing thumbnails on S3", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, true);
+        };
+
+        s3 = nock('https://s3.amazonaws.com')
+          .head('/FAKES3/desktop/large/t.makes.org/blah')
+          .reply(200);
+
+        request(app)
+          .get('/desktop/large/t.makes.org/blah')
+          .expect('Location', 'https://s3.amazonaws.com' +
+                              '/FAKES3/desktop/large/t.makes.org/blah')
+          .expect(302)
+          .end(done);
+      });
+
+      it("uses blitline for nonexistent thumbnails", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, true);
+        };
+
+        blitlineScreenshot = function(options, cb) {
+          options.should.eql({
+            "appId": "FAKE",
+            "s3bucket": "FAKES3",
+            "url": "https://t.makes.org/blah_",
+            "thumbnails": [{
+              "height": 240,
+              "s3key": "desktop/small/t.makes.org/blah",
+              "width": 320
+            }, {
+              "height": 384,
+              "s3key": "desktop/large/t.makes.org/blah",
+              "width": 512
+            }],
+            "viewport": {
+              "height": 600,
+              "width": 800
+            },
+            "wait": 3000
+          });
+          cb(null);
+        };
+
+        s3 = nock('https://s3.amazonaws.com')
+          .head('/FAKES3/desktop/large/t.makes.org/blah')
+          .reply(404);
+
+        request(app)
+          .get('/desktop/large/t.makes.org/blah')
+          .expect('Location', 'https://s3.amazonaws.com' +
+                              '/FAKES3/desktop/large/t.makes.org/blah')
+          .expect(302)
+          .end(done);
+      });
+
+      it("propagates blitline errors", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, true);
+        };
+
+        blitlineScreenshot = function(options, cb) {
+          cb(new Error("darn"));
+        };
+
+        s3 = nock('https://s3.amazonaws.com')
+          .head('/FAKES3/desktop/large/t.makes.org/blah')
+          .reply(404);
+
+        request(app)
+          .get('/desktop/large/t.makes.org/blah')
+          .expect(500)
+          .expect("darn")
+          .end(done);
+      });
     });
 
-    it("propagate errors when looking up makes", function(done) {
-      verifyIsHtml = function(url, cb) {
-        cb(new Error("blah"));
-      };
+    describe("POST /:viewport/:key", function() {
+      it("returns 400 when make doesn't exist", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, false);
+        };
 
-      s3 = nock('https://s3.amazonaws.com')
-        .head('/FAKES3/desktop/large/t.makes.org/blah')
-        .reply(404);
+        request(app)
+          .post('/desktop/large/t.makes.org/blah')
+          .expect(400)
+          .expect({error: 'URL is not HTML'})
+          .end(done);
+      });
 
-      request(app)
-        .get('/desktop/large/t.makes.org/blah')
-        .expect(500)
-        .expect("blah")
-        .end(done);
-    });
+      it("returns 200 on screenshot success", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, true);
+        };
 
-    it("return 200 for existing thumbnails on S3", function(done) {
-      verifyIsHtml = function(url, cb) {
-        cb(null, true);
-      };
+        blitlineScreenshot = function(options, cb) {
+          cb(null);
+        };
 
-      s3 = nock('https://s3.amazonaws.com')
-        .head('/FAKES3/desktop/large/t.makes.org/blah')
-        .reply(200);
+        request(app)
+          .post('/desktop/large/t.makes.org/blah')
+          .expect(200)
+          .expect({screenshot: 'https://s3.amazonaws.com' +
+                               '/FAKES3/desktop/large/t.makes.org/blah'})
+          .end(done);
+      });
 
-      request(app)
-        .get('/desktop/large/t.makes.org/blah')
-        .expect('Location', 'https://s3.amazonaws.com' +
-                            '/FAKES3/desktop/large/t.makes.org/blah')
-        .expect(302)
-        .end(done);
-    });
+      it("propagates blitline errors", function(done) {
+        verifyIsHtml = function(url, cb) {
+          cb(null, true);
+        };
 
-    it("uses blitline for nonexistent thumbnails", function(done) {
-      verifyIsHtml = function(url, cb) {
-        cb(null, true);
-      };
+        blitlineScreenshot = function(options, cb) {
+          cb(new Error("doh"));
+        };
 
-      blitlineScreenshot = function(options, cb) {
-        options.should.eql({
-          "appId": "FAKE",
-          "s3bucket": "FAKES3",
-          "url": "https://t.makes.org/blah_",
-          "thumbnails": [{
-            "height": 240,
-            "s3key": "desktop/small/t.makes.org/blah",
-            "width": 320
-          }, {
-            "height": 384,
-            "s3key": "desktop/large/t.makes.org/blah",
-            "width": 512
-          }],
-          "viewport": {
-            "height": 600,
-            "width": 800
-          },
-          "wait": 3000
-        });
-        cb(null);
-      };
-
-      s3 = nock('https://s3.amazonaws.com')
-        .head('/FAKES3/desktop/large/t.makes.org/blah')
-        .reply(404);
-
-      request(app)
-        .get('/desktop/large/t.makes.org/blah')
-        .expect('Location', 'https://s3.amazonaws.com' +
-                            '/FAKES3/desktop/large/t.makes.org/blah')
-        .expect(302)
-        .end(done);
-    });
-
-    it("propagates blitline errors", function(done) {
-      verifyIsHtml = function(url, cb) {
-        cb(null, true);
-      };
-
-      blitlineScreenshot = function(options, cb) {
-        cb(new Error("darn"));
-      };
-
-      s3 = nock('https://s3.amazonaws.com')
-        .head('/FAKES3/desktop/large/t.makes.org/blah')
-        .reply(404);
-
-      request(app)
-        .get('/desktop/large/t.makes.org/blah')
-        .expect(500)
-        .expect("darn")
-        .end(done);
+        request(app)
+          .post('/desktop/large/t.makes.org/blah')
+          .expect(500)
+          .expect("doh")
+          .end(done);
+      });
     });
   });
 
